dff447b05ba8ac7e9acf30ebc9632b24c7d9cd3c
joaopedrocg27 <jpcg89@gmail.com>
2017-07-07 08:19:45 +0100

    Implement speech recognition

---
+++ speech.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+
+def transcribe(language='en-US'):
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    try:
+        return r.recognize_google(audio, language=language)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        try:
+            return r.recognize_sphinx(audio, language=language)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        try:
+            print('Trying with sphinx')
+            return r.recognize_sphinx(audio, language=language)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
