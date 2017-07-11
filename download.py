93754d80c17ad804ea6c4ddcdca4c24c2ccf7bef
gmes78 <gmes.078@gmail.com>
2017-07-11 16:26:20 +0100

    Make the download function output a file with the video id as the name

--- download.py
+++ download.py
@@ -2,6 +2,6 @@ import youtube_dl
 
 
 def download(video_id):
-    ydl_opts = {}
+    ydl_opts = {'outtmpl': '%(id)s.%(ext)s'}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['https://youtube.com/watch?v=' + video_id])
