b1c6720cc82facb50234e23865052199c6b9306b
gmes78 <gmes.078@gmail.com>
2017-07-11 16:32:06 +0100

    Search for videos only

--- search.py
+++ search.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 
 def search(text):
     query = urllib.parse.quote(text)
-    url = "https://www.youtube.com/results?search_query=" + query
+    # The '&sp=EgIQAQ%253D%253D' is used to only search for videos.
+    url = "https://www.youtube.com/results?search_query=" + query + '&sp=EgIQAQ%253D%253D'
     response = urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html)
     
-    return soup.find(attrs={'class':'yt-uix-tile-link'})['href'][9:]
+    return soup.find(attrs={'class': 'yt-uix-tile-link'})['href'][9:]
