579e1be97e34918445e2da33bd7b98768c3ca3bf
gmes78 <gmes.078@gmail.com>
2017-07-12 15:52:26 +0100

    Use voice recognition

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
+import sys
 import search
 import download
 import player
+import speech
 
 
 def main():
+    argv = ' '.join(map(str, sys.argv[1:]))
+    if argv != '':
+        use_voice = False
+    else:
+        use_voice = True
     while True:
-        search_terms = input('Music to play: ')
+        if argv != '':
+            search_terms = argv
+            argv = ''
+        elif not use_voice:
+            search_terms = input('Music to play: ')
+        else:
+            print('Listening...')
+            search_terms = speech.transcribe()
+
         video_id = search.search(search_terms)
 
         download.download(video_id)
