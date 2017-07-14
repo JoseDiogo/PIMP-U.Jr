b484931a35e3fc46f6df6453d513a19c8dc60d33
gmes78 <gmes.078@gmail.com>
2017-07-14 11:10:35 +0100

    Add Python <3.5 support

--- download.py
+++ download.py
@@ -2,5 +2,9 @@ import subprocess
 
 
 def download(video_id):
-    subprocess.run(['youtube-dl', '-x', '--audio-quality', '0', '--audio-format', 'flac', '-o', '%(id)s.%(ext)s',
-                    'https://youtube.com/watch?v=' + video_id])
+    try:
+        subprocess.run(['youtube-dl', '-x', '--audio-quality', '0', '--audio-format', 'flac', '-o', '%(id)s.%(ext)s',
+                        'https://youtube.com/watch?v=' + video_id])
+    except AttributeError:
+        subprocess.call(['youtube-dl', '-x', '--audio-quality', '0', '--audio-format', 'flac', '-o', '%(id)s.%(ext)s',
+                        'https://youtube.com/watch?v=' + video_id])
